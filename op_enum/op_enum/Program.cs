de82b95f17bc33b76c2275ff1b07b8bc9762124c
ThienLeDuc <anhchangnhonhan020904@gmail.com>
2025-02-23 21:04:01 +0700

    optional paramerer and enum

---
+++ op_enum/op_enum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace op_enum
+{
+    class Program
+    {
+        enum Infor
+        {
+            name_138,
+            id_138
+        }
+
+        // Phương thức Student phải là static vì Main() là static
+        private static string Student(Infor infor)
+        {
+            switch (infor)
+            {
+                case Infor.name_138:
+                    return "Le Duc Thien";
+                case Infor.id_138:
+                    return "22115053122138";
+                default:
+                    return "answer...";
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Name: " + Student(Infor.name_138));
+            Console.WriteLine("ID: " + Student(Infor.id_138));
+
+            Console.ReadLine(); 
+        }
+    }
+}
