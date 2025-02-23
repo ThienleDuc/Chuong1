12d9f33c4cc9ae3637965255eb12d7619877c99d
ThienLeDuc <anhchangnhonhan020904@gmail.com>
2025-02-23 20:26:37 +0700

    Optional parameter

---
+++ optional_parameter/optional_parameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace optional_parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name_138 = "Le Duc Thien";
+            String id_138 = "22115053122138";
+
+            Console.WriteLine("Name: " + name_138);
+            Console.WriteLine("Id: " + id_138);
+            Console.ReadLine();
+        }
+    }
+}
