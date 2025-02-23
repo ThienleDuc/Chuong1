4dbc06e88c0daacd611f4a53a050749926f06945
ThienLeDuc <anhchangnhonhan020904@gmail.com>
2025-02-23 20:48:27 +0700

    Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    class Program
+    {
+        enum Infor
+        {
+          id01_138 = 2,
+          id02_138 = 2,
+          id12_138 = 1,
+          id13_138 = 3,
+          id14_138 = 8
+        }
+
+        static void Main(string[] args)
+        {
+            string id_138 = $"{(int)Infor.id01_138}{(int)Infor.id02_138}...{(int)Infor.id12_138}{(int)Infor.id13_138}{(int)Infor.id14_138}";
+            Console.WriteLine("Id: " + id_138);
+            Console.ReadLine();
+        }
+    }
+}
